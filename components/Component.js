e4133247d060f9a7c69e23c77f8e0bea7828feae
Anupam <anupam@10-16-190-204.dynapool.wireless.nyu.edu>
2023-03-31 03:42:17 -0400

    version-2

--- components/Component.js
+++ components/Component.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export default function Component(props) {
     return (
         <>
-             <div class="flex flex-col items-center justify-end p-3 rounded-lg drop-shadow-md bg-white border-2 border-b-[#bfbaba] hover:bg-[#f7f6f6] " key={props.item.info}>
+             <div class="flex flex-col items-center justify-end p-3 rounded-lg drop-shadow-md bg-white border-2 border-b-[#bfbaba] hover:bg-[#f7f6f6] " >
                 <div class="flex justify-center h-full">
-                    <img class="sm:w-6/2 lg:w-6/12" src={props.item.item_picture} alt={"https://logos-world.net/wp-content/uploads/2021/09/NYU-Logo-700x394.png"}/>
-                    </div>
+                <img class="sm:w-6/2 lg:w-6/12" src={props.item.item_picture} alt={"https://logos-world.net/wp-content/uploads/2021/09/NYU-Logo-700x394.png"}/>
+                </div>
                 <div className='p-4 w-full  hover:text-[#58048c]'>
                 <h3 class="font-medium py-2">Item: {props.item.item_name}</h3>
                 <h3 className='font-light'>Category: {props.item.cat}</h3>
@@ -14,8 +14,6 @@ export default function Component(props) {
                 <p className='font-light'>Description: {props.item.item_desc}</p>
                 <p className='font-light'>Quantity: {props.item.item_quantity}</p>
                 <p className='font-light'>Data Sheet: {props.item.item_datasheet}</p>
-
-        
                 </div>
             </div>
         </>
--- components/Results.js
+++ components/Results.js
@@ -6,10 +6,12 @@ export default function Main(props) {
         <>
             <div class="p-5" >
                 {list_items && <div className='grid m-auto grid-cols-3 gap-4'>{list_items.map((item) => (
-                    <Component item={item} ></Component>
+                    <Component key={Math.floor(Math.random() * 1000000)} item={item} ></Component>
                 ))}
                 </div>}
             </div>
         </>
     )
 }
+
+//key={props.item.item_name}
